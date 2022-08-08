ed6dbe4798d8de9b5d543497b4c9ad6d8715d2fe
abdulmujeeeb <bdulmujeebidrees@gmail.com>
2022-08-08 17:14:46 +0100

    commit message

---
+++ script.js
@@ -0,0 +1,49 @@
+//number1
+let quote =
+    "'There is no exercise better for the heart than reaching down and lifting people up.' by John Holmes teaches us to help one another.'";
+
+console.log(quote);
+
+
+//number2
+let secondquote =
+    "Love is not patronizing and charity isn't about pity, it is about love. Charity and love are the same -- with charity you give love, so don't just give money but reach out your hand instead.";
+
+console.log(quote);
+
+//number3
+console.log(typeof 10 === 10);
+let tenstring = "10";
+let tennumber = number("10");
+
+//number4
+let float = "9.8";
+letnumber = Number;
+console.log(Math.ceil(float));
+
+//number5
+console.log("python".includes(on) && "jargon".includes("on"));
+
+//6
+console.log("i hope this course is not full of jargon".includes("jargon"));
+
+//7
+let number = Math.floor(Math.random() * 101);
+console.log(number);
+
+//8
+let numb = Math.floor(Math.random() * 50) + 50;
+console.log(numb);
+
+//9
+let num = Math.floor(Math.random() * 256);
+console.log(num);
+
+//10
+//11
+
+//12
+const sentence =
+    "you cannot end a sentence with because because because is a conjunction";
+console.log(sentence.substr(31, 23));
+
